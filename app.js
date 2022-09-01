377ed17bf35aefef87312e3d4b27f1a14d85b2e1
shoaib <shoaibahmed2@live.com>
2022-09-01 15:28:04 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+var myTable = prompt("write a number for the table");
+var Table = Number(myTable);
+var i;
+var Table;
+for (var i_1 = 1; i_1 < 11; i_1++) {
+    // console.log(' 5 x ' + i + '=' ,Table*i)
+    console.log("".concat(Table, " * ").concat(i_1, " =  ").concat(Table * i_1, " "));
+}
---
+++ app.ts
@@ -0,0 +1,16 @@
+var myTable:null | string = prompt("write a number for the table");
+var Table = Number(myTable);
+
+var i:number;
+var Table:number;
+
+for (let i = 1; i < 11; i++) {
+        
+   // console.log(' 5 x ' + i + '=' ,Table*i)
+   console.log( `${Table} * ${i} =  ${Table * i} `);
+    
+    
+    
+    
+    
+}
